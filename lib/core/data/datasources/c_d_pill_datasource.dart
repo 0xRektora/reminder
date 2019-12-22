4ebc75ed0de68a526344aa394b3bdee15617398d
Mendi Walid <Bloodyline01@gmail.com>
2019-12-22 18:34:04 -0500

    Implemented deletePill datasource & exceptions handling

--- lib/core/data/datasources/c_d_pill_datasource.dart
+++ lib/core/data/datasources/c_d_pill_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:reminder/core/error/exceptions.dart';
 import 'package:reminder/core/error/failures.dart';
 import 'package:reminder/core/static/c_s_db_routes.dart';
 import 'package:reminder/features/prescriptions/domain/entities/f_pill_entity.dart';
@@ -7,6 +8,7 @@ import 'package:reminder/features/prescriptions/domain/entities/f_pill_entity.da
 import '../models/c_d_app_pill_model.dart';
 
 abstract class CDPillDatasource {
+  Future<bool> deletePill({@required String uid, @required String pillName});
   Future<CDAppPillModel> getPill(
       {@required String uid, @required String pillName});
   Future<List<CDAppPillModel>> allPill({@required String uid});
@@ -19,40 +21,63 @@ class CDPillDatasourceImpl implements CDPillDatasource {
   Future<bool> addPill(
       {@required CDAppPillModel appPillModel, @required String uid}) async {
     try {
-      final result = await Firestore.instance
+      await Firestore.instance
           .collection(CSDbRoutes.PRESCRIPTIONS)
           .document(uid)
           .collection(CSDbPillDoc.PATH)
-          .add(appPillModel.toDoc());
-      if (result.documentID.length != 0) {
-        return true;
-      } else {
-        return false;
-      }
+          .document(appPillModel.pillName)
+          .setData(appPillModel.toDoc());
+      return true;
     } on Exception catch (e) {
-      throw ServerFailure(message: e.toString());
+      throw ServerException(message: e.toString());
     }
   }
 
   @override
   Future<List<CDAppPillModel>> allPill({String uid}) async {
-    final ds = await Firestore.instance
-        .collection(CSDbRoutes.PRESCRIPTIONS)
-        .document(uid)
-        .collection(CSDbPillDoc.PATH)
-        .getDocuments();
-    final listDs = ds.documents.map((ds) => ds.data).toList();
-    return CDAppPillModel.fromListSnapshot(listDs);
+    try {
+      final ds = await Firestore.instance
+          .collection(CSDbRoutes.PRESCRIPTIONS)
+          .document(uid)
+          .collection(CSDbPillDoc.PATH)
+          .getDocuments();
+      final listDs = ds.documents.map((ds) => ds.data).toList();
+      return CDAppPillModel.fromListSnapshot(listDs);
+    } on Exception catch (e) {
+      throw ServerException(message: e.toString());
+    }
   }
 
   @override
   Future<CDAppPillModel> getPill({String uid, String pillName}) async {
-    final ds = await Firestore.instance
-        .collection(CSDbRoutes.PRESCRIPTIONS)
-        .document(uid)
-        .collection(CSDbPillDoc.PATH)
-        .document(pillName)
-        .get();
-    return CDAppPillModel.fromSnapshot(ds.data);
+    try {
+      final ds = await Firestore.instance
+          .collection(CSDbRoutes.PRESCRIPTIONS)
+          .document(uid)
+          .collection(CSDbPillDoc.PATH)
+          .document(pillName)
+          .get();
+      return CDAppPillModel.fromSnapshot(ds.data);
+    } on Exception catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<bool> deletePill({
+    @required String uid,
+    @required String pillName,
+  }) async {
+    try {
+      await Firestore.instance
+          .collection(CSDbRoutes.PRESCRIPTIONS)
+          .document(uid)
+          .collection(CSDbPillDoc.PATH)
+          .document(pillName)
+          .delete();
+      return true;
+    } on Exception catch (e) {
+      throw ServerException(message: e.toString());
+    }
   }
 }
