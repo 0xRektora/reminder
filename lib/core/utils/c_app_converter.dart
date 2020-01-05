1878def686b6aecf882181f575ec2dd460e33af1
Walid Mendi <bloodyline01@gmail.com>
2020-01-05 14:43:33 -0500

    changed datetime functions to time and added datetime

--- lib/core/utils/c_app_converter.dart
+++ lib/core/utils/c_app_converter.dart
@@ -1,9 +1,17 @@
 class CAppConverter {
-  static String fromDatetimeToString(DateTime dateTime) {
+  static String fromTimeToString(DateTime dateTime) {
     return "${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}";
   }
 
-  static DateTime fromStringToDatetime(String dateTime) {
+  static DateTime fromStringToTime(String dateTime) {
     return DateTime.parse("1970-01-01 $dateTime:00.00");
+  }
+
+  static String fromDatetimeToString(DateTime dateTime) {
+    return "${dateTime.year}-${dateTime.month}-${dateTime.day}";
+  }
+
+  static DateTime fromStringToDatetime(String dateTime) {
+    return DateTime.parse("$dateTime 00:00:00.00");
   }
 }
