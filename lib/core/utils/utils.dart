93d177556f740bbef020e53e7b86f6562fe3ba52
Walid Mendi <bloodyline01@gmail.com>
2020-01-05 11:05:56 -0500

    cut functions to Shared Pref Manager

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,15 +1,1 @@
-import 'dart:convert';
 
-import 'package:dartz/dartz_unsafe.dart';
-
-class PackPayload {
-  static String call(Map<String, dynamic> data) {
-    return jsonEncode(data);
-  }
-}
-
-class UnpackPayload {
-  static Map<String, dynamic> call(String payload) {
-    return jsonDecode(payload);
-  }
-}
