72db7e6dee1ff54a03e3e99f5bd54c41de5d4702
Walid Mendi <bloodyline01@gmail.com>
2020-01-06 20:17:26 -0500

    cleaned to follow the architecture tree

--- lib/features/reminder_presc/domain/usecases/f_reminder_presc_validate_usecase.dart
+++ lib/features/reminder_presc/domain/usecases/f_reminder_presc_validate_usecase.dart
@@ -2,11 +2,12 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:reminder/core/data/models/c_d_app_pill_model.dart';
-import 'package:reminder/core/error/failures.dart';
-import 'package:reminder/core/usecases/usecase.dart';
-import 'package:reminder/features/prescriptions/domain/entities/f_pill_entity.dart';
-import 'package:reminder/features/reminder_schedule/domain/repositories/f_reminder_schedule_repo.dart';
+
+import '../../../../core/data/models/c_d_app_pill_model.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../../../prescriptions/domain/entities/f_pill_entity.dart';
+import '../../../reminder_schedule/domain/repositories/f_reminder_schedule_repo.dart';
 
 class FReminderPrescValidateUsecase
     extends Usecase<bool, FReminderPrescValidateUsecaseParam> {
@@ -18,18 +19,9 @@ class FReminderPrescValidateUsecase
   Future<Either<Failure, bool>> call(
       FReminderPrescValidateUsecaseParam params) async {
     try {
-      final CDAppPillModel cdAppPillModel = CDAppPillModel(
-        current: params.fpPillEntity.current,
-        pillName: params.fpPillEntity.pillName,
-        qtyToTake: params.fpPillEntity.qtyToTake,
-        remindAt: params.fpPillEntity.remindAt,
-        remindWhen: params.fpPillEntity.remindWhen,
-        taken: params.fpPillEntity.taken,
-        total: params.fpPillEntity.total,
-      );
       return await fReminderScheduleRepo.validate(
         uid: params.uid,
-        cdAppPillModel: cdAppPillModel,
+        fpPillEntity: params.fpPillEntity,
         flutterLocalNotificationsPlugin: params.flutterLocalNotificationsPlugin,
       );
     } on InternalFailure catch (e) {
