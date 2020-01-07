e13ba5bc6447496dc816c1c4158dbfa4f93a3f66
Walid Mendi <bloodyline01@gmail.com>
2020-01-06 20:18:28 -0500

    working on the UI for feature reminder presc

--- lib/features/reminder_presc/presentation/bloc/reminder_presc_bloc.dart
+++ lib/features/reminder_presc/presentation/bloc/reminder_presc_bloc.dart
@@ -1,9 +1,20 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
 import './bloc.dart';
+import '../../domain/usecases/f_reminder_presc_list_validate.dart';
+import '../../domain/usecases/f_reminder_presc_validate_usecase.dart';
 
 class ReminderPrescBloc extends Bloc<ReminderPrescEvent, ReminderPrescState> {
-  final 
+  final FReminderPrescListValidateUsecase fReminderPrescListValidateUsecase;
+  final FReminderPrescValidateUsecase fReminderPrescValidateUsecase;
+
+  ReminderPrescBloc({
+    @required this.fReminderPrescListValidateUsecase,
+    @required this.fReminderPrescValidateUsecase,
+  });
   @override
   ReminderPrescState get initialState => InitialReminderPrescState();
 
@@ -13,6 +24,10 @@ class ReminderPrescBloc extends Bloc<ReminderPrescEvent, ReminderPrescState> {
   ) async* {
     if (event is FReminderPrescListEvent) {
       // TODO yield list state
+    }
+
+    if (event is FReminderPrescValidateEvent) {
+      // TODO validate the entry
     }
   }
 }
--- lib/features/reminder_presc/presentation/pages/reminder_presc_page.dart
+++ lib/features/reminder_presc/presentation/pages/reminder_presc_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reminder/features/reminder_presc/presentation/bloc/bloc.dart';
+
+import '../../../../dependency_injector.dart';
 
 class ReminderPrescPage extends StatefulWidget {
   ReminderPrescPage({Key key}) : super(key: key);
@@ -8,8 +12,24 @@ class ReminderPrescPage extends StatefulWidget {
 }
 
 class _ReminderPrescPageState extends State<ReminderPrescPage> {
+  void _blocStateBuilder(BuildContext context, ReminderPrescState state) {}
+
+  void _blocListener(BuildContext context, ReminderPrescState state) {}
+
+  Widget _blocBuilder(BuildContext context) {
+    return BlocListener<ReminderPrescBloc, ReminderPrescState>(
+      listener: _blocListener,
+      child: BlocBuilder<ReminderPrescBloc, ReminderPrescState>(
+        builder: _blocStateBuilder,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider<ReminderPrescBloc>(
+      create: (_) => sl<ReminderPrescBloc>(),
+      child: _blocBuilder(context),
+    );
   }
 }
