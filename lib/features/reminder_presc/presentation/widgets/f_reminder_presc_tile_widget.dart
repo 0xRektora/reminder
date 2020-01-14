31c8d2893f8f00f8f3a906c3c083d33b63115969
Walid Mendi <bloodyline01@gmail.com>
2020-01-13 20:00:34 -0500

    implementing notification tile validator widget

--- lib/features/reminder_presc/presentation/widgets/f_reminder_presc_tile_widget.dart
+++ lib/features/reminder_presc/presentation/widgets/f_reminder_presc_tile_widget.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../../domain/entities/f_reminder_presc_presc_notification_entity.dart';
+
 class FRprescTileWidget extends StatefulWidget {
-  FRprescTileWidget({Key key}) : super(key: key);
+  final FReminderPrescPrescNotificationEntity prescNotificationEntity;
+  FRprescTileWidget({
+    Key key,
+    @required this.prescNotificationEntity,
+  }) : super(key: key);
 
   @override
   _FRprescTileWidgetState createState() => _FRprescTileWidgetState();
@@ -10,6 +16,12 @@ class FRprescTileWidget extends StatefulWidget {
 class _FRprescTileWidgetState extends State<FRprescTileWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: ListTile(
+        title: Text(widget.prescNotificationEntity.notificationName),
+        trailing: Text(
+            '${widget.prescNotificationEntity.timeToTake.hour}:${widget.prescNotificationEntity.timeToTake.minute}'),
+      ),
+    );
   }
 }
