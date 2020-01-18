10f8155fe7c3a10f187fc12382d827655b506826
Mendi Walid <Bloodyline01@gmail.com>
2020-01-18 12:32:29 -0500

    added field to UserModel

--- lib/features/login/data/models/login_model.dart
+++ lib/features/login/data/models/login_model.dart
@@ -2,9 +2,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:reminder/core/data/user.dart';
 
 class UserModel extends User {
-  UserModel(String uid, String name) : super(uid, name);
+  UserModel(
+    String uid,
+    String name, {
+    String creationDate,
+  }) : super(uid, name);
 
-  factory UserModel.fromFirebaseUser(FirebaseUser firebaseUser) {
-    return UserModel(firebaseUser.uid, firebaseUser.displayName);
+  factory UserModel.fromFirebaseUser(
+    FirebaseUser firebaseUser, {
+    String creationDate,
+  }) {
+    return UserModel(
+      firebaseUser.uid,
+      firebaseUser.displayName,
+      creationDate: creationDate,
+    );
   }
 }
